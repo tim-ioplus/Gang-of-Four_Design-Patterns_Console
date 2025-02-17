cb1d01cd02e68c6e856c36cec30b74ed54900088
tim-ioplus <138361783+tim-ioplus@users.noreply.github.com>
2025-02-17 11:39:57 +0100

    add Memento Pattern Runner

---
+++ VB/Behavioural/Memento/MementoPatternRunner.vb
@@ -0,0 +1,201 @@
+﻿Public Class MementoPatternRunner
+
+	Public Sub Run()
+		Console.WriteLine("Memento Pattern...")
+
+		Dim currentPixelValue As Integer = 0
+		Dim expectedValue As Integer = 0
+		' neues Bitmap: 2d arr decimal
+		Dim editor = New BitmapEditor(5, 5)
+		' init mit mit Helligkeitswerten 0-255
+		editor.InitGradient()
+		currentPixelValue = editor.GetValue(0, 0)
+		expectedValue = 128
+		CheckValues(currentPixelValue, expectedValue)
+		' 10pkt heller machen
+		editor.Brighter(10)
+		currentPixelValue = editor.GetValue(0, 0)
+		expectedValue = 138
+		CheckValues(currentPixelValue, expectedValue)
+		' 10pkt heller machen 
+		editor.Brighter(10)
+		currentPixelValue = editor.GetValue(0, 0)
+		expectedValue = 148
+		CheckValues(currentPixelValue, expectedValue)
+		' 5pkt dunkler machen
+		editor.Darker(5)
+		currentPixelValue = editor.GetValue(0, 0)
+		expectedValue = 143
+		CheckValues(currentPixelValue, expectedValue)
+		' zurücksetzen
+		editor.Undo()
+		currentPixelValue = editor.GetValue(0, 0)
+		expectedValue = 148
+		CheckValues(currentPixelValue, expectedValue)
+		'
+		editor.Undo()
+		currentPixelValue = editor.GetValue(0, 0)
+		expectedValue = 138
+		CheckValues(currentPixelValue, expectedValue)
+		'
+		editor.Undo()
+		currentPixelValue = editor.GetValue(0, 0)
+		expectedValue = 128
+		CheckValues(currentPixelValue, expectedValue)
+
+	End Sub
+
+	Private Sub CheckValues(current As Integer, expected As Integer)
+		Dim areEqual As Boolean = current.Equals(expected)
+
+		Dim areEqualText = String.Format("{0} And {1} Are ", current, expected)
+		If areEqual Then
+			areEqualText += " Equal "
+		Else
+			areEqualText += " Not Equal "
+		End If
+
+		Console.WriteLine(areEqualText)
+	End Sub
+End Class
+
+Public Class Bitmap
+	Dim _pixels(,) As Integer
+	Dim _width As Integer = 0
+	Dim _height As Integer = 0
+	Public Sub New(ByVal width As Integer, ByVal height As Integer)
+		_width = width
+		_height = height
+		ReDim _pixels(width - 1, height - 1)
+	End Sub
+
+	Public Function GetValue(x As Integer, y As Integer) As Integer
+		If x <= _width And y <= _height Then
+			Return _pixels(x, y)
+		End If
+
+		Return Nothing
+	End Function
+
+	Public Sub SetValue(x As Integer, y As Integer, value As Integer)
+		_pixels(x, y) = value
+	End Sub
+
+	Public Sub SetValues(ByVal newValues As Integer(,))
+		Array.Copy(newValues, Me._pixels, Me._pixels.Length)
+	End Sub
+
+	Public Function Clone() As Bitmap
+		Dim copy As New Bitmap(Me._width, Me._height)
+		copy.SetValues(Me._pixels)
+
+		Return copy
+	End Function
+
+
+End Class
+Public Class BitmapEditor
+
+	Dim _bitmap As Bitmap
+	Dim _history As History
+
+	Public Sub New(width As Integer, height As Integer)
+		_bitmap = New Bitmap(width, height)
+		_history = New History
+	End Sub
+
+	Public Sub Restore(newBitmap As Bitmap)
+		_bitmap = newBitmap
+	End Sub
+
+	Public Function GetValue(i As Integer, j As Integer) As Integer
+		Return _bitmap.GetValue(i, j)
+	End Function
+
+	Public Sub InitGradient()
+		Save()
+
+		For i As Integer = 0 To 4
+			For j As Integer = 0 To 4
+				Dim count = (i + j)
+				Dim brightness = 128 + count
+
+				_bitmap.SetValue(i, j, brightness)
+			Next
+		Next
+
+	End Sub
+
+	Public Sub Brighter(increaseValue As Integer)
+		Save()
+
+		For i As Integer = 0 To 4
+			For j As Integer = 0 To 4
+				Dim value = _bitmap.GetValue(i, j)
+				value += increaseValue
+				_bitmap.SetValue(i, j, value)
+			Next
+		Next
+
+	End Sub
+
+	Public Sub Darker(decreaseValue As Integer)
+		Save()
+
+		For i As Integer = 0 To 4
+			For j As Integer = 0 To 4
+				Dim value = _bitmap.GetValue(i, j)
+				value -= decreaseValue
+				_bitmap.SetValue(i, j, value)
+			Next
+		Next
+
+	End Sub
+
+	Public Sub Save()
+		Dim cloneToSave = _bitmap.Clone
+		_history.Save(cloneToSave)
+	End Sub
+
+	Public Sub Undo()
+		Dim lastMemento As Memento = _history.Undo
+
+		If lastMemento IsNot Nothing Then
+			Dim lastState = TryCast(lastMemento.State, Bitmap)
+			If lastState IsNot Nothing Then
+				Restore(lastState)
+			End If
+		End If
+
+	End Sub
+
+End Class
+
+Public Class History
+	Dim _history As Stack(Of Memento)
+
+	Public Sub New()
+		_history = New Stack(Of Memento)
+	End Sub
+
+	Public Sub Save(ByVal state As Object)
+		_history.Push(New Memento(state))
+	End Sub
+
+	Public Function Undo() As Memento
+		If _history.Count > 0 Then
+			Return _history.Pop
+		End If
+
+		Return Nothing
+	End Function
+
+End Class
+
+Public Class Memento
+	Public State As Object
+
+	Public Sub New(ByVal state As Object)
+		Me.State = state
+	End Sub
+End Class
--- VB/Module1.vb
+++ VB/Module1.vb
@@ -23,21 +23,53 @@ Module Module1
 		'Dim chainOfResponsibilityPatternRunner As New ChainOfResponsibilityPatternRunner()
 		'chainOfResponsibilityPatternRunner.Run()
 
+		' 3.2 Command
+		' Einzelne atomare operationen aus den zustand eines objektes ausführen und bei Bedarf
+		' wieder rückggängig machen zu können, transaktionsartig
+		'Dim commandPatternRunner As New CommandPatternRunner
+		'CommandPatternRunner.Run()
+
+		'3.3 Iterator
+		'
+		'Dim iteratorPatternRunner As New IteratorPatternRunner()
+		'IteratorPatternRunner.Run()
+
+
+		' 3.4 Mediator 
+		' 
+		' Struktur um Objekte miteinander interagieren, kommunizieren zu lassen ohne sie direkt aufzurufen
+		' - Kommunikation zwischen GUI Elemnten um Eingaben und Änderungen der Zustände zu kommunizieren
+		' - Chatsysteme
+		'
+		'Dim mediatorPatternRunner = New MediatorPatternRunner
+		'mediatorPatternRunner.Run()
+
+
+		'// 3.5 Memento
+		'//
+		'// Zwischenspeichern eines Objektzustands um darauf folgende Änderungen wieder rückgängig machen zu können.
+		'// - Texteditor > Text, Formatierungen
+		'// - Grafikbearbeitung > Objekte, Pixel, Farbe
+		'//
+
+		Dim mementoPatternRunner = New MementoPatternRunner
+		mementoPatternRunner.Run()
+
+		'3. Observer
+
+
+		'3.x State 
+		'Dim statePatternRunner As New StatePatternRunner()
+		'StatePatternRunner.Run()
+
 		' 3.x Strategy
 		'Dim strategyPatternRunner As New StrategyPatternRunner()
 		'strategyPatternRunner.Run()
 
-		'3.x Iterator
-		'Dim iteratorPatternRunner As New IteratorPatternRunner()
-		'iteratorPatternRunner.Run()
 
-		'3.x State 
-		Dim statePatternRunner As New StatePatternRunner()
-		statePatternRunner.Run()
-
-
-		Console.WriteLine("exit in 10 secs")
-		Thread.Sleep(10000)
+		Dim secs As Integer = 5000
+		Console.WriteLine($"exit in {secs / 1000} secs")
+		Thread.Sleep(secs)
 
 	End Sub
 
